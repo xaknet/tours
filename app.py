61bca579c481afb3eb732ad5c83e747ff03ca50a
Eldar <eldar219@gmail.com>
2020-02-15 19:54:08 -0500

    Working version but still need to improve the menu with functionality

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 import data
+from data import tours
 from flask import request
 
 app = Flask(__name__)
@@ -13,24 +14,66 @@ app.config['SECRET_KEY'] = 'YTOIHWEFHGSI'
 
 @app.route('/')
 def main():
+    limit_items = 6
     return render_template('index.html',
                            title=data.title,
                            description=data.description,
                            subtitle=data.subtitle,
-                           departures=data.departures)
+                           departures=data.departures,
+                           tours=data.tours,
+                           limit_items=limit_items)
 
 
 @app.route('/departure/<departure>')
 def departure(departure):
+    keys_list = []
+    tours_list = []
+    tours_dict = {}
+    for key, value in tours.items():
+        if departure == value.get("departure"):
+            tours_list.append(value)
+            tours_dict[key] = value
+        else:
+            print("Item not found")
+
     return render_template("departure.html",
                            departures=data.departures,
                            title=data.title,
-                           departure=departure)
+                           departure=departure,
+                           tours_list=tours_list,
+                           keys_list=keys_list,
+                           tours_dict=tours_dict,
+                           tours=tours.items())
 
 
-@app.route('/tour/<id>')
+@app.route('/tour/<int:id>')
 def tour(id):
-    return render_template("tour.html")
+    if id in tours.keys():
+        id = tours.get(id)
+    else:
+        return not_found(404)
+
+    current_country = id.get("departure")
+    from_city = data.departures.get(current_country)
+
+
+    output = render_template("tour.html",
+                             id=id,
+                             title=id.get("title"),
+                             description=id.get("description"),
+                             departure=id.get("departure"),
+                             picture=id.get("picture"),
+                             price=id.get("price"),
+                             stars=int(id.get("stars")),
+                             country=id.get("country"),
+                             nights=id.get("nights"),
+                             date=id.get("date"),
+                             departures=data.departures,
+                             current_country=current_country,
+                             from_city=from_city,
+                             )
+
+    return output
 
 
 @app.errorhandler(404)
